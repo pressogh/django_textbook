85ee292a17a04eb0a495eeb209334ec02b7249a9
pressogh <caff1nepill@gmail.com>
2022-04-12 15:34:59 +0900

    Add more desc in textbook lv2-1

--- djangostudy/board/boardapp/admin.py
+++ djangostudy/board/boardapp/admin.py
@@ -1,3 +1,9 @@
+# < boardapp/admin.py >
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('create_time', )
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- djangostudy/board/boardapp/models.py
+++ djangostudy/board/boardapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
