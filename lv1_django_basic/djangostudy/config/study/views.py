421661e74009cb39d44d9ebd1ffcd88fb9a212f4
pressogh <caff1nepill@gmail.com>
2022-04-12 12:48:01 +0900

    Update lv1 textbook

--- lv1_django_basic/djangostudy/config/study/views.py
+++ lv1_django_basic/djangostudy/config/study/views.py
@@ -5,5 +5,10 @@ from django.http import HttpResponse    # Http 응답을 반환해주는 함수
 
 # HelloView로 요청이 오면
 def HelloView(request):
-    # Http 응답 반환
-    return HttpResponse('Hello World!')
+    # dict 형태의 data를 만들어줌
+    data = {
+        'texts': ["python", "django", "study", "template tag"],
+    }
+
+    # 템플릿을 render해서 반환
+    return render(request, 'study/index.html', data)
